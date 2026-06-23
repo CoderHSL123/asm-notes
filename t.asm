0a9fbf063e3d68536d9e4b405485fbbd868eca52
hsl <2276524341@qq.com>
2026-06-24 01:48:27 +0800

    实验7代码

--- t.asm
+++ t.asm
@@ -1,44 +1,96 @@
-assume cs:code,ds:data,ss:stack
+assume cs:code, ds:data, ss:stack
 
-;数据段
 data segment
-	dw 0123H,0456H,0789H,0ABCH,0DEFH,0FEDH,0CBAH,0987H
+	db '1975','1976','1977','1978','1979','1980','1981','1982'
+	db '1983','1984','1985','1986','1987','1988','1989','1990'
+	db '1991','1992','1993','1994','1995'
+	; 以上是21年的21个字符串
+	dd 16,22,382,1356,2390,8000,16000,24486,50065,97479,140417,197514
+	dd 345980,590827,803530,1183000,1843000,2759000,3753000,4649000,5937000
+	; 以上是21年间每年的总收入
+	dw 3,7,9,13,28,38,130,220,476,778,1001,1442,2258,2793,4037,5635,8226
+	dw 11542,14430,15257,17800
+	; 以上是21年间每年的雇员人数
 data ends
 
+table segment
+	db 21 dup('year sumn ne ?? ')
+table ends
+
+
+
 stack segment stack
-	dw 0,0,0,0,0,0,0,0
 	dw 0,0,0,0,0,0,0,0
 stack ends
 
 
 
+; 将data段中的每个单词的前4个字母改为大写字母
 code segment
-;start伪指令将程序入口地址信息记录在最终的exe文件中的描述信息中
-;程序启动时会依照描述信息对CS IP寄存器进行设置
 start:
-		mov ax,stack
-		mov ss,ax
-		
-		mov ax,data
-		mov ds,ax
+	mov ax, data
+	mov ds, ax
 
-		mov bx,0
-		mov cx,8
-pushData:
-		push ds:[bx]
-		add bx,2
-		loop pushData
+	mov ax, stack
+	mov ss, ax
+	mov sp, 10h
 
-		mov ax,4C00H
-		int 21H
-		
+	mov ax, table
+	mov es, ax
+
+	mov si, 0
+	mov bx, 0	; 代表数据项的起始地址
+	mov bp, 0	; 代表table的行
+	mov di, 0	; 代表雇员人数的下标
+	add di, 0
+	mov cx, 21
+process:
+	; 复制年
+	push cx
+	; 按字节复制年
+	mov cx, 4
+	mov si, 0
+copyYear:
+	mov al, ds:[bx + si]
+	mov es:[bp + si], al
+	inc si
+	loop copyYear
 
 
+
+
+	; 复制总收入
+	mov si, 0
+	mov cx, 4
+copyIncome:
+	mov al, ds:[bx + 21*4 + si]
+	mov es:[bp + 5 + si], al
+	inc si
+
+	loop copyIncome
+
+	; 复制雇员人数
+
+		; 雇员人数直接以字为单位进行复制
+	mov ax, ds:[42*4 + di]
+	mov es:[bp + 10], ax
+
+
+	; 计算平均工资
+	mov ax, es:[bp + 5]
+	mov dx, es:[bp + 7]
+	div word ptr es:[bp + 10]
+	mov es:[bp + 0dh], ax
+
+	add di, 2
+	add bx, 4
+	add bp, 16
+	pop cx
+	loop process
+
+
+	mov ax,4c00H
+	int 21h
 code ends
 
-
-
-
-
 end start
-
--- t1.asm
+++ t1.asm
@@ -1,52 +1,78 @@
-assume cs:code,ds:data,ss:stack
+assume cs:code, ds:data, ss:stack
 
 data segment
-	a db 1,2,3,4,5,6,7,8 ; 数据标号a：字节单元，起始地址：data:0
-	b dw 0			  	 ; 数据标号b：字单元，起始地址：data:8
-	c dw a,b			 ; 将标号地址作为数据存储，相当于dw offset a, offset b
-						 ; 若使用双字存储（如'dd a,b'），会保存偏移地址和段地址
+	db '1975','1976','1977','1978','1979','1980','1981','1982'
+	db '1983','1984','1985','1986','1987','1988','1989','1990'
+	db '1991','1992','1993','1994','1995'
+	; 以上是21年的21个字符串
+	dd 16,22,382,1356,2390,8000,16000,24486,50065,97479,140417,197514
+	dd 345980,590827,803530,1183000,1843000,2759000,3753000,4649000,5937000
+	; 以上是21年间每年的总收入
+	dw 3,7,9,13,28,38,130,220,476,778,1001,1442,2258,2793,4037,5635,8226
+	dw 11542,14430,15257,17800
+	; 以上是21年间每年的雇员人数
 data ends
+
+table segment
+	db 21 dup('year sumn ne ?? ')
+table ends
+
 
 
 stack segment stack
-	db 128 dup(0)
+	dw 0,0,0,0,0,0,0,0
 stack ends
 
 
 
-
-
+; 将data段中的每个单词的前4个字母改为大写字母
 code segment
-
 start:
-		mov ax,data 	; 补全，设置ds段寄存器为data段的段地址，方便访问data段中的数据
-		mov ds,ax		; 必须显示设置，否则会报错
-		mov ax,stack
-		mov ss,ax
-		mov sp,128
-		
-		mov si,0
-		mov cx,8
-calculate:
-		mov al,a[si]	; 访问data段数据 mov al,es:[si]
-		mov ah,0
-		add b,ax
-		inc si
-		loop calculate
+	mov ax, data
+	mov ds, ax
 
-		mov ax,4C00H
-		int 21H
+	mov ax, stack
+	mov ss, ax
+	mov sp, 10h
 
-;============显示内容的代码=============
-show:
-		mov ax,0b800h
-		mov es,ax
-		mov al,39h
-		mov byte ptr es:[12*160+40*2], al ;屏幕显示感叹号
-		ret
+	mov si, 0			; 定义0索引
+	mov di, 21*4 + 21*4	; 定义总收入的开始索引
 
+	mov ax, table
+	mov es, ax
+	mov bp, 0
+	mov cx, 21
 
+copyElement:
+	; 完成单个年份
+	push ds:[si + 0]
+	pop es:[bp + 0]
+	push ds:[si + 2]
+	pop es:[bp + 2]
+
+	; 完成收入复制
+	push ds:[si + 21*4 + 0]
+	pop es:[bp + 5]
+	push ds:[si + 21*4 + 2]
+	pop es:[bp + 7]
+
+	; 完成总收入的复制
+	push ds:[di]
+	pop es:[bp + 10]
+
+	; 计算每年每人的平均工资
+	mov ax, es:[bp + 5]
+	mov dx, es:[bp + 7]
+	div word ptr es:[bp + 10]
+	mov es:[bp + 13], ax
+
+	add si, 4
+	add di, 2
+	add bp, 16
+	loop copyElement
+
+	mov ax,4c00H
+	int 21h
 code ends
-
 
 end start
