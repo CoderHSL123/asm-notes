ae0239d8a0928a81a7a194100402fed74fd6ec31
hsl <2276524341@qq.com>
2026-06-26 18:07:00 +0800

    实验10-1显示字符串

--- t1.asm
+++ t1.asm
@@ -1,7 +1,8 @@
 assume cs:code, ds:data, ss:stack
-
+;==========================实验10.1显示字符串
 data segment
-	db 'welcome to masm!'
+	db 'hello hsl, hello world!',0
+	db 'nihao shijie wo shi hsl',0
 data ends
 
 
@@ -15,6 +16,30 @@ stack ends
 
 ; 将data段中的每个单词的前4个字母改为大写字母
 code segment
+start:
+	; 1.初始化栈，因为要调用函数
+	mov ax, stack
+	mov ss, ax
+	mov sp, 16
+
+	; 2.初始化寄存器
+	call init_reg
+	; 3.设置行和列
+	mov dl, 1			; 行 0-24
+	mov dh, 13			; 列 0-79
+	; 4.调用计算列
+	call get_col		; 将行与列转化成列号，结果存在bx中
+
+	add bx, bx			; 将列号乘以2，因为每个字符占用2个字节
+
+	mov cl, 10000010b	; 颜色，此处为绿色
+	mov si, 0			; 指向字符串首地址的指针
+
+	call showString
+
+	mov ax,4c00H
+	int 21h
+init_reg:
 
 	mov ax, data
 	mov ds, ax
@@ -22,33 +47,40 @@ code segment
 	mov ax, 0B800H
 	mov es, ax
 
-	; 将data段中的字符输出到屏幕
-	mov si, 0
-	; 向25 * 80的屏幕中间写入字符串  25是行数，80是列数
-	; 写入的位置应为40行，12列
-	mov di, (12*80 + 32 -1) * 2
+	ret
 
-	mov ah, 00000010b
-	mov cx, 16
 
-printChar:
-	mov al, ds:[si]
-	mov es:[di], ax
+;===================计算行对应的列数，将结果存入bx中
+get_col:
+	mov al, dl
+	mov ah, 80
+	mul ah
+	mov dl, dh
+	mov dh, 0
+	add ax, dx
+	mov bx, ax
+	ret
+
+
+showString:
+
+
+	mov ah, cl				; 保存颜色属性
+putChar:
+	mov ch, 0
+	mov cl, ds:[si]			; 获取要输出的字符
+	jcxz retFunc			; 如果cx为0，那么就说明到了字符串的结束，返回函数
+
+	mov es:[bx], cl			; 显示字符
+	mov es:[bx+1], ah		; 显示颜色属性
 	inc si
-	add di, 2
-	loop printChar
+	add bx, 2
+	jmp short putChar
 
-	mov ax,4c00H
-	int 21h
 
-start:
-; 使用retf指令修改cs:ip，使其返回到code segment开始执行程序
-	mov ax, cs
-	push ax
+retFunc:
+	ret
 
-	mov ax, 0
-	push ax
-	retf
 code ends
 
 end start
