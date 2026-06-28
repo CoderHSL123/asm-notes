fbba063e7504efba33a1942a49b84660c997a1a5
hsl <2276524341@qq.com>
2026-06-28 17:36:50 +0800

    实验11 编写子程序

--- t1.asm
+++ t1.asm
@@ -1,10 +1,10 @@
 assume cs:code, ds:data, ss:stack
-;==========================实验10.3数值显示
+;==========================实验11编写子程序
+;编写一个子程序，将包含任意字符，
+;以0结尾的字符串中的小写字母转变成大写字母
 
 data segment
-	dw 32145
-	db 14 dup(0)
-	db 16 dup(0)
+	db "Beginner's All-purpose Symbolic Instruction Code.",0
 
 data ends
 
@@ -26,147 +26,54 @@ start:
 	; 2.初始化寄存器
 	call init_reg
 
-	call convertNumToString
+	; 3.调用子程序
+	call letterc
 
-	; 将栈中的数据弹出，存入数据段中
-	call popStack
-	; 3.调用函数显示字符串
-	call displayString
 
 	; 4.退出程序
 	mov ax,4c00H
 	int 21h
 
-;====================初始化寄存器
-init_reg:
 
+;==========================初始化寄存器
+init_reg:
 	mov ax, data
 	mov ds, ax
-	mov ax, 0B800H
-	mov es, ax
-
 
 	mov si, 0
-	ret
-
-;======================将数字转换为字符串存入栈中
-convertNumToString:
-	; 保存栈顶地址
-	mov bp, sp
-	mov ax, data
-	mov ss, ax
-	mov sp, 32
-
-
-	mov bx, 10
-	mov ax, ds:[si]  		; 从数据段中获取数字 低位
-	mov dx, 0				; 从数据段中获取数字 高位
-
-traverseBit:
-	div bx
-	; 根据商是否为0，判断是否已经遍历完所有位
-	mov cx, ax
-	; 将余数转换为字符
-	add dx, 48
-	push dx
-	inc byte ptr ds:[2]
-	jcxz done
-	mov dx, 0				; 清空余数寄存器
-	jmp short traverseBit
-
-done:
-	; 保存ds部分的栈顶地址
-	mov ds:[4], sp
-	; 恢复栈顶地址
-	mov sp, bp
-	mov ax, stack
-	mov ss, ax
 
 	ret
-;======================将栈中的数据弹出，存入数据段中
-popStack:
-	mov cx, ds:[2]
-	mov si, 16
 
-	mov bp, sp
-	mov ax, data
-	mov ss, ax
-	; 恢复ds部分的栈顶地址
-	mov sp, ds:[4]
 
-	; 输出每个字符到屏幕
-loopBit:
-	pop ax
+
+
+;==========================将字符串中的小写字母转变成大写字母
+letterc:
+	; 通过si依次取出每个字符，判断字符是否是小写字母
+	; 如果是小写字母，就将其转换为大写字母
+	; 如果不是小写字母，就直接跳过
+	; 最后，将转换后的字符串放回data段
+
+	mov al, ds:[si]
+	; 判断该字符是否是结束符
+	cmp al, 0
+	je retFunc
+	
+	cmp al, 'a'
+	jb notSmallLetter
+	cmp al, 'z'
+	ja notSmallLetter
+	sub al, 'a' - 'A'
+	; 将转换后的字符放回data段
 	mov ds:[si], al
+notSmallLetter:
 	inc si
-	loop loopBit
-
-	mov byte ptr ds:[si], 0	; 字符串的结束标志
-	mov sp, bp
-	mov ax, stack
-	mov ss, ax
-	ret
-
-;======================显示字符串
-displayString:
-	push ax
-	push bx
-	push cx
-	push dx
-	push si
-	push di
-	push bp
-
-	mov dl, 4	; 行
-	mov dh, 40	; 列
-
-	call get_col
-	add bx, bx			; 将列号乘以2，因为每个字符占用2个字节
-
-	mov cl, 10000010b	; 颜色，此处为绿色
-	mov si, 10H			; 指向字符串首地址的指针
-
-	call ShowString
-
-	pop bp
-	pop si
-	pop di
-	pop cx
-	pop dx
-	pop bx
-	pop ax
-
-	ret
-
-;===================计算行对应的列数，将结果存入bx中
-get_col:
-	mov al, dl
-	mov ah, 80
-	mul ah
-	mov dl, dh
-	mov dh, 0
-	add ax, dx
-	mov bx, ax
-	ret
-
-ShowString:
-
-	mov ah, cl				; 保存颜色属性
-putChar:
-	mov ch, 0
-	mov cl, ds:[si]			; 获取要输出的字符
-	jcxz retFunc			; 如果cx为0，那么就说明到了字符串的结束，返回函数
-
-	mov es:[bx], cl			; 显示字符
-	mov es:[bx+1], ah		; 显示颜色属性
-	inc si
-	add bx, 2
-	jmp short putChar
-
+	jmp letterc
 
 retFunc:
-	ret
 
+	; 退出子程序
+	ret
 
 
 code ends
