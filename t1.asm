73433f55bc03ae56b37b08302b0e4d2e622c146b
hsl <2276524341@qq.com>
2026-06-30 21:50:35 +0800

    检测点13.2 使用中断实现jmp near ptr s的功能

--- t1.asm
+++ t1.asm
@@ -1,9 +1,9 @@
 assume cs:code, ds:data, ss:stack
-; 出现除法错误的时候，触发中断执行我们定义的函数
-; 修改中断向量表的0号中断向量，指向我们定义的函数
+; 修改中断号7CH的中断向量，指向我们定义的函数
+; 使的data段中的字符串的所有字符都改为大写字母
 
 data segment
-	db 'over flow!', 0
+	db 'conversation hello world', 0
 	db 16 dup(0)
 
 data ends
@@ -17,24 +17,6 @@ stack ends
 
 
 code segment
-	; 中断处理程序
-
-interruptFunc:
-displayString:
-	mov ch, 0
-	mov cl, ds:[si]
-	jcxz funcRet
-	mov ch, 00000010b	; 设置字符为黑底绿字
-	mov es:[di], cx
-	add di, 2
-	inc si
-	jmp short displayString
-
-funcRet:
-	; 返回dos系统
-	mov ax, 4c00h
-	int 21h
-
 
 start:
 	; 1.初始化栈，因为要调用函数
@@ -48,11 +30,24 @@ start:
 	; 3.修改中断向量表的0号中断向量，指向我们定义的函数
 	call modify_ivt
 
-	; 4.执行一个除法错误
-	mov ax, 1000
-	mov bl, 0
-	div bl
+	; 获得displayStringEnd与displayString的偏移位移，并将结果存放在bx中，
+	; 用于后续的中断例程中跳转
+	mov bx, offset displayString - offset displayStringEnd
+	
+	mov ah, 00000010b
+displayString:
+	mov al, ds:[si]
+	cmp al, 0
+	je endProgram
+	mov es:[di], ax
+	add di, 2
+	inc si
+	int 7CH
+displayStringEnd:
+	nop
 
+
+endProgram:
 	; 5.退出程序
 	mov ax,4c00H
 	int 21h
@@ -62,10 +57,13 @@ start:
 init_reg:
 	mov ax, data
 	mov ds, ax
-	mov ax, 0B800H
+	mov si, 0
+	
+	mov ax,0B800H
 	mov es, ax
-	mov si, 0	; 代表字符串的起始地址
-	mov di, 880	; 在终端显示的列的位置，记得要乘以2，因为每个字符占用2个字节
+	mov di, 12 * 160	;从第12行开始显示
+
+
 	ret
 
 
@@ -77,12 +75,27 @@ modify_ivt:
 	mov ax, 0
 	mov ds, ax
 	; ip的地址
-	mov word ptr ds:[0], offset interruptFunc
+	mov word ptr ds:[7ch * 4], offset myNearJmp
 	; cs的地址
 	mov ax, code
-	mov ds:[0+2], ax
+	mov ds:[7ch * 4 + 2], ax
 	pop ds
 	ret
+
+
+
+
+;==========================使用中断例程函数模拟jmp near ptr跳转
+myNearJmp:
+
+	; 将ds段中的字符串显示到终端中
+	push bp
+	mov bp, sp
+	; 计算出的位移增加到IP中
+	add word ptr ss:[bp + 2], bx
+	mov sp, bp
+	pop bp
+	iret
 
 
 code ends
