acc0d01ab1683be854e7cec08237fb6b99050371
hsl <2276524341@qq.com>
2026-07-03 00:10:30 +0800

    按下esc键修改字符颜色

--- t1.asm
+++ t1.asm
@@ -1,9 +1,9 @@
 assume cs:code, ds:data, ss:stack
-; 修改中断号7CH的中断向量，指向我们定义的函数
-; 使的data段中的字符串的所有字符都改为大写字母
+; 修改现有的9号中断向量，
+; 使其在键盘按下esc键后，更改整个屏幕的颜色
 
 data segment
-	db 'conversation hello world', 0
+	db 16 dup(0)
 	db 16 dup(0)
 
 data ends
@@ -23,31 +23,44 @@ start:
 	mov ax, stack
 	mov ss, ax
 	mov sp, 32
+	
 
 	; 2.初始化寄存器
 	call init_reg
 
-	; 3.修改中断向量表的0号中断向量，指向我们定义的函数
-	call modify_ivt
+	; 3.保存原来中断向量的IP CS
+	mov ax, 0
+	mov ds, ax
 
-	; 获得displayStringEnd与displayString的偏移位移，并将结果存放在bx中，
-	; 用于后续的中断例程中跳转
-	mov bx, offset displayString - offset displayStringEnd
-	
-	mov ah, 00000010b
-displayString:
-	mov al, ds:[si]
-	cmp al, 0
-	je endProgram
-	mov es:[di], ax
-	add di, 2
-	inc si
-	int 7CH
-displayStringEnd:
-	nop
+	push ds:[9 * 4 + 2]			; 保存CS
+	push ds:[9 * 4]				; 保存IP
+	; 恢复ds寄存器指向data段
+	mov ax, data
+	mov ds, ax
+
+	pop ds:[0]					; 保存IP
+	pop ds:[2]					; 保存CS
+
+	; 4.修改9号中断向量
+	mov ax, 0
+	mov ds, ax
+	mov bx, offset int9
+	mov ds:[9 * 4], bx				; 修改IP
+	mov ds:[9 * 4 + 2], cs			; 修改CS
+
+	mov ax, data
+	mov ds, ax
+
+	; 打印字母表
+	mov ah, 'a'
+printAlphabet:
+	mov es: [160 *12 + 40 *2], ah
+	call delay
+	inc ah
+	cmp ah, 'z'
+	jna printAlphabet
 
 
-endProgram:
 	; 5.退出程序
 	mov ax,4c00H
 	int 21h
@@ -57,45 +70,75 @@ endProgram:
 init_reg:
 	mov ax, data
 	mov ds, ax
-	mov si, 0
-	
-	mov ax,0B800H
+
+	mov ax, 0B800H
 	mov es, ax
-	mov di, 12 * 160	;从第12行开始显示
-
+	
 
 	ret
 
 
 
-
-;==========================修改中断向量表的0号中断向量，指向我们定义的函数
-modify_ivt:
-	push ds
-	mov ax, 0
-	mov ds, ax
-	; ip的地址
-	mov word ptr ds:[7ch * 4], offset myNearJmp
-	; cs的地址
-	mov ax, code
-	mov ds:[7ch * 4 + 2], ax
-	pop ds
-	ret
-
-
-
-
-;==========================使用中断例程函数模拟jmp near ptr跳转
-myNearJmp:
-
-	; 将ds段中的字符串显示到终端中
+int9:
+	; 1.保存上下文寄存器
+	push ax
+	push bx
+	push cx
+	push dx
+	push si
+	push di
 	push bp
-	mov bp, sp
-	; 计算出的位移增加到IP中
-	add word ptr ss:[bp + 2], bx
-	mov sp, bp
+
+
+	in al, 60h
+	push ax
+	pushf
+	call dword ptr ds:[0]
+	pop ax
+	; 比较是否是esc键
+	cmp al, 01h
+	jne continue
+
+	; 修改4000个单元的颜色
+	mov cx, 2000
+	mov bx, 1
+changeColor:
+	; 修改颜色
+	inc byte ptr es:[bx]
+	add bx, 2
+	loop changeColor
+
+
+
+continue:
 	pop bp
+	pop di
+	pop si
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+
 	iret
+
+
+delay:
+	push ax
+	push dx
+	mov dx, 10h
+	mov ax, 0h
+s1:
+	sub ax, 1
+	sbb dx, 0
+	cmp ax, 0
+	jne s1
+
+	cmp dx, 0
+	jne s1
+
+	pop dx
+	pop ax
+	ret
 
 
 code ends
